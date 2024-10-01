bac72116b628ed82270e7d60e4b222cb93af11c9
Emir Inan <emirinandev@gmail.com>
2024-10-01 18:12:27 +0300

    vercel speed insights added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import "./App.css";
 import KanbanBoard from "./components/KanbanBoard";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 function App() {
-  return <KanbanBoard />;
+  return (
+    <>
+      <KanbanBoard />;
+      <SpeedInsights />;
+    </>
+  );
 }
 
 export default App;
